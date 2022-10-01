cf77bd8f29f46fe80e400a3090369d00ffc0ced8
dervanovicaisa <aisa.dervanovic@ninjacloud.rs>
2022-10-02 01:19:13 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 // import SideMenu from "./layouts/SideMenu";
 import ButtonView from "./views/ButtonView";
 import InputView from "./views/InputView";
 
 function App() {
-  const navigate = useNavigate();
   return (
     <Routes>
-      <Route
-        exact
-        path="/"
-        render={() => {
-          return navigate("/input");
-        }}
-      >
-        <Route path="/button" element={<ButtonView />} />
-        <Route path="/input" element={<InputView />} />
-      </Route>
+      <Route exact path="/" element={<Navigate to="/input" />} />
+      <Route path="/button" element={<ButtonView />} />
+      <Route path="/input" element={<InputView />} />
     </Routes>
   );
 }
